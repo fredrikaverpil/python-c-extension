2dc5db092b8f86c887df84c94eeec6b7f0d13601
Fredrik Averpil <fredrik@averpil.com>
2021-09-23 10:22:55 +0200

    Add comments

--- hello.c
+++ hello.c
@@ -1,22 +1,27 @@
 #include <Python.h>
 
-static PyObject* _hello_world(PyObject *self, PyObject *args) {
-    return PyUnicode_FromString("Hello, world!");  // python c api
+static PyObject *_hello_world(PyObject *self, PyObject *args) {
+    // self is a reference to the c extension itself
+    return PyUnicode_FromString("Hello, world!"); // google "python c api" for more info
 };
 
 static struct PyMethodDef methods[] = {
+    // array of method definitions
+
+    // method name, function pointer (cast to PyCFunction), flags
     {"hello_world", (PyCFunction)_hello_world, METH_NOARGS},
-    {NULL, NULL}
+    {NULL, NULL}  // null terminated array
 };
 
 static struct PyModuleDef module = {
+    // module definition
     PyModuleDef_HEAD_INIT,
-    "hello",
+    "hello",  // name matching the module in python
     NULL,
     -1,
-    methods
-};
+    methods};
 
 PyMODINIT_FUNC PyInit_hello(void) {
+    // public initialization of the module, what is being called when importing the module
     return PyModule_Create(&module);
 }
