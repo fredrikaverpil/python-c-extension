275e707f7b7b062a4c98211b8d67bc079aeb167b
Fredrik Averpil <fredrik@averpil.com>
2021-09-23 14:55:39 +0200

    Run pytest

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 import hello
 
-ret = hello.hello_world()
-assert ret == "hello world"
 
-ret = hello.greet("fredrik")
-assert ret == "hello FREDRIK"
+def test_hello():
+    assert hello.hello_world() == "hello world"
+
+
+def test_greet():
+    assert hello.greet("fredrik") == "hello FREDRIK"
